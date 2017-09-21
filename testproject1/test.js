ee3aaa24ef3a33fd353f02af831b4f7c06a1f939
delchev <delchevn@gmail.com>
2017-09-21 18:49:43 +0300

    auth type added

--- testproject1/test.js
+++ testproject1/test.js
@@ -2,7 +2,9 @@ var request = require('http/v3/request');
 var response = require('http/v3/response');
 
 var method = request.getMethod();
+var authType = request.getAuthType();
 
 response.println("[Method]: " + method);
+response.println("[Auth Type]: " + authType);
 response.flush();
 response.close();
