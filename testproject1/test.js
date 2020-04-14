e343393503735f9d066521ffc110c6dfc10feb53
delchev <delchevn@gmail.com>
2020-04-15 02:58:45 +0300

    method comment

--- testproject1/test.js
+++ testproject1/test.js
@@ -1,7 +1,7 @@
 var request = require('http/v3/request');
 var response = require('http/v3/response');
 
-var method = request.getMethod();
+var method = request.getMethod(); // method
 var authType = request.getAuthType(); // auth type
 
 response.println("[Method]: " + method);
