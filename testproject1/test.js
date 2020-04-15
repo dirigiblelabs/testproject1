278e3a8686338ac2dfee248c01022ab4a1890daf
Nedelcho Delchev <delchevn@gmail.com>
2020-04-15 03:00:14 +0300

    Update test.js

--- testproject1/test.js
+++ testproject1/test.js
@@ -4,7 +4,7 @@ var response = require('http/v3/response');
 var method = request.getMethod(); // method
 var authType = request.getAuthType(); // auth type
 
-response.println("[Method]: " + method);
+response.println("[Method]: " + method); // print method
 response.println("[Auth Type]: " + authType);
 response.flush();
 response.close();
